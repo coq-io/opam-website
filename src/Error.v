34485b1aac0dbe9de582f2f39a4f49e3614636e7
Guillaume Claret <dev@clarus.me>
2015-03-22 17:16:39 +0100

    Run for the errors

--- src/Error.v
+++ src/Error.v
@@ -1,3 +1,4 @@
+Require Import FunctionNinjas.All.
 Require Import Io.All.
 
 Import C.Notations.
@@ -34,3 +35,41 @@ Definition raise {E : Effect.t} {Exc A : Type} (exc : Exc)
   : C.t (effect E Exc) A :=
   let! absurd := C.Call (effect E Exc) (Command.Exc exc) in
   match absurd with end.
+
+Module Tree.
+  Inductive t (Exc : Type) :=
+  | Exc (exc : Exc)
+  | Join (x y : t Exc).
+  Arguments Exc [Exc] _.
+  Arguments Join [Exc] _ _.
+End Tree.
+
+Fixpoint run {E : Effect.t} {Exc A : Type} (x : C.t (effect E Exc) A)
+  : C.t E (A + Tree.t Exc) :=
+  match x with
+  | C.Ret _ x => ret @@ inl x
+  | C.Call (Command.Ok c) =>
+    let! answer := C.Call E c in
+    ret @@ inl answer
+  | C.Call (Command.Exc exc) => ret @@ inr @@ Tree.Exc exc
+  | C.Let _ _ x f =>
+    let! x := run x in
+    match x with
+    | inl x => run (f x)
+    | inr exc => ret @@ inr exc
+    end
+  | C.Join _ _ x y =>
+    let! xy := join (run x) (run y) in
+    match xy with
+    | (inl x, inl y) => ret @@ inl (x, y)
+    | (inr exc, inl _) | (inl _, inr exc) => ret @@ inr exc
+    | (inr exc_x, inr exc_y) => ret @@ inr @@ Tree.Join exc_x exc_y
+    end
+  | C.First _ _ x y =>
+    let! xy := first (run x) (run y) in
+    match xy with
+    | inl (inl x) => ret @@ inl @@ inl x
+    | inr (inl y) => ret @@ inl @@ inr y
+    | inl (inr exc) | inr (inr exc) => ret @@ inr exc
+    end
+  end.
